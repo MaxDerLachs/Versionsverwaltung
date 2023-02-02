95cebb084c5bce534904aa4fd749afbbaa5116f6
MaxDerLachs <84570313+MaxDerLachs@users.noreply.github.com>
2023-02-02 17:58:10 +0100

    Add files via upload

---
+++ Versionsverwaltung/Versionsverwaltung/src/main/java/SE/Versionsverwaltung/VersionsverwaltungApplication.java
@@ -0,0 +1,13 @@
+package SE.Versionsverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionsverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionsverwaltungApplication.class, args);
+	}
+
+}
