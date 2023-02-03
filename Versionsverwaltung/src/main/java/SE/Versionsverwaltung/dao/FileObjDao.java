a839a5ca30c76d07803fb7c8976521074845efcd
mkam <maximilian.kaminski@d-velop.de>
2023-02-03 06:07:05 +0100

    data access object methods

--- Versionsverwaltung/src/main/java/SE/Versionsverwaltung/dao/FileObjDao.java
+++ Versionsverwaltung/src/main/java/SE/Versionsverwaltung/dao/FileObjDao.java
@@ -1,18 +1,15 @@
 package SE.Versionsverwaltung.dao;
 
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import SE.Versionsverwaltung.model.FileObj;
-
+import com.mongodb.client.*;
 import org.bson.Document;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 public interface FileObjDao {
+    String uri = "mongodb+srv://maxikami:dVmonsq4559iB5JL@cluster1.eot2cys.mongodb.net/?retryWrites=true&w=majority";
 
-    public String uri = "mongodb+srv://maxikami:dVmonsq4559iB5JL@cluster1.eot2cys.mongodb.net/?retryWrites=true&w=majority";
-    public static void getFileByID(String fileID) {
+    static FileObj getFileObjByID(String fileID) {
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("files_db");
@@ -20,10 +17,38 @@ public interface FileObjDao {
 
             Document doc = collection.find(eq("fileID", fileID)).first();
             if (doc != null) {
-                System.out.println(doc.toJson());
+                return generateFileObj(doc);
             } else {
                 System.out.println("No matching documents found.");
+                return null;
             }
         }
+    }
+
+    static FileObj getFileObjByNameAndVersion(String fileName, String fileVersion){
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("files_db");
+            MongoCollection<Document> collection = database.getCollection("files_collection");
+
+            Document doc = collection.find(and(eq("fileName", fileName),eq("fileVersion", fileVersion))).first();
+            if (doc != null) {
+                return generateFileObj(doc);
+            } else {
+                System.out.println("No matching documents found.");
+                return null;
+            }
+        }
+    }
+
+    static FileObj generateFileObj(Document doc){
+        return new FileObj(
+                doc.getString("fileID"),
+                doc.getString("fileName"),
+                doc.getString("fileText"),
+                doc.getString("fileVersion"),
+                doc.getString("fileParent"),
+                doc.getString("fileEditor"),
+                doc.getBoolean("fileBlocked"));
     }
 }
--- Versionsverwaltung/src/main/java/SE/Versionsverwaltung/dao/TestFileObjDao.java
+++ Versionsverwaltung/src/main/java/SE/Versionsverwaltung/dao/TestFileObjDao.java
@@ -3,6 +3,8 @@ package SE.Versionsverwaltung.dao;
 public class TestFileObjDao {
     public static void main( String[] args ) {
 
-        FileObjDao.getFileByID("0001");
+        System.out.println(FileObjDao.getFileObjByID("0002").getFileName());
+        System.out.println(FileObjDao.getFileObjByNameAndVersion("test_001","1.0.0").getFileID());
+        System.out.println(FileObjDao.getFileObjByNameAndVersion("test_001","1.1.0").getFileID());
     }
 }
